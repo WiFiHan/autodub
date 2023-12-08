393ca7619cf58fd22f3e70d914df9e8e5818545a
MintyProtein <dnltjdwls1@naver.com>
2023-12-08 13:06:11 +0000

    add TODO: speech enhancement

--- autodub/tts.py
+++ autodub/tts.py
@@ -8,7 +8,27 @@ from autodub.VALL_E_X.utils.generation import SAMPLE_RATE, generate_audio
 from scipy.io.wavfile import write as write_wav
 
 
-def prepare_prompts(script, name):
+
+def enhance_speech(audio_clip_dir:str|os.PathLike):
+    # TODO: Implement Speech Enhancement
+    '''
+    Enhance and replace all audio files in given dir.
+    '''
+    raise NotImplementedError()
+
+def prepare_prompts(script:pd.DataFrame, name:str, enhance:bool=False):
+    '''
+    Generate and save prompt files.
+    
+    Parameters:
+        script ('pd.DataFrame'): 
+            Script containing time data of each line. 
+            Might be generated from 'autodub.stt.STT.get_script_from_video()'
+        
+        name ('str'): To get audio and output dirs
+        
+        enhance ('bool'): Whether or not to use speech enhancement
+    '''
     audio_clip_dir = f"./results/{name}/audio/source/"
     prompt_dir = f"./results/{name}/prompt/"
     os.makedirs(f"./results/{name}/prompt/source", exist_ok=True)
@@ -16,6 +36,10 @@ def prepare_prompts(script, name):
     for idx, row in tqdm(script.iterrows(), total=script.shape[0], desc="Generating prompts.."):
         audio_clip_path = audio_clip_dir + f"/segment_{str(idx).zfill(6)}.wav"
         prompt_path = prompt_dir + f"/prompt_{str(idx).zfill(6)}.npz"
+        
+        if enhance:
+            enhance_speech(audio_clip_dir)
+            
         prompt = make_prompt(name=name,
                     audio_path=audio_clip_path,
                     transcript=row['source']
